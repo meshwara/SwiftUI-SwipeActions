e9852923d292bb240874f48d8f5d7e260b86b4e3
D. Prameswara <meshwara@gmail.com>
2023-01-04 17:00:24 +0700

    Initial Commit

---
+++ TestSwipeActions/TestSwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwipeActionsApp.swift
+//  TestSwipeActions
+//
+//  Created by D. Prameswara on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TestSwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
