96d5c5e5af51b93ef8e28b7bc9155b3acbe361f5
D. Prameswara <meshwara@gmail.com>
2023-01-05 05:29:50 +0700

    Update swipe

--- TestSwipeActions/ContentView.swift
+++ TestSwipeActions/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var info = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(info)")           
+            List {
+                ForEach(0 ..< 10, id: \.self) { i in
+                    Text("Item number \(i)")
+                        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                            Button {
+                                info = "Mark read item \(i)"
+                            } label: {
+                                Label(
+                                    "Mark read",
+                                    systemImage: "envelope.open.fill"
+                                )
+                            }
+                            .tint(.blue)
+
+                            Button {
+                                info = "Reply item \(i)"
+                            } label: {
+                                Label(
+                                    "Reply",
+                                    systemImage: "envelope.arrow.triangle.branch.fill"
+                                )
+                            }
+                            .tint(.green)
+                            
+                            Button {
+                                info = "Favorite item \(i)"
+                            } label: {
+                                Text(
+                                    "Favorite"
+                                )
+                            }
+                            .tint(.brown)
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                info = "Archive item \(i)"
+                            } label: {
+                                Label(
+                                    "Archive",
+                                    systemImage: "archivebox.fill"
+                                )
+                            }
+                            .tint(.cyan)
+                            
+                            
+                        }
+                }
+            }
         }
-        .padding()
     }
 }
 
